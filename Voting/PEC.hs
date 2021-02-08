f1a5057293ad4fc93125010fe3999e499a558323
Galen Huntington <galen@alumni.reed.edu>
2021-02-07 19:31:44 -0800

    Migrate into own repo.

---
+++ Voting/PEC.hs
@@ -0,0 +1,101 @@
+module Voting.PEC where
+
+--+
+import BasePrelude
+import Data.Vector as V
+import Data.Vector.Generic as VG
+
+
+type Cand = Int
+type Count = Integer
+type Label = String
+type CountVec = V.Vector Count
+type BallotSet = [(Label, CountVec)]
+
+data PECInput = PECInput { _electors :: !Count, _votes :: CountVec }
+   deriving stock (Eq)
+
+instance Show PECInput where
+   show (PECInput els vs) =
+      show els ++ " -> " ++ concat (intersperse ":" $ map show $ toList vs)
+
+apportion :: Count -> CountVec -> CountVec
+''  el vs = loop (VG.replicate (VG.length vs) 0) (sum vs % el) where
+   loop lt sv =
+      let es = fmap ((/ sv) . (%1)) vs; f_es = fmap floor es in
+      case sum f_es `compare` el of
+         EQ -> f_es
+         LT ->
+            let upf = maximum $ VG.zipWith (\e f -> e / (f%1+1)) es f_es
+            in loop f_es (sv * upf)
+         GT -> lt
+
+addAtLarge :: Count -> PECInput -> CountVec -> CountVec
+''  atl inp els =
+   let vv = _votes inp 
+       maxes = findIndices (\x -> x == VG.maximum vv) $ toList vv
+       es = atl `div` genericLength maxes
+   in VG.unsafeAccum (+) els $ map (\i -> (i, es)) maxes
+
+--  Range of legal quotas.
+--  confusing API, needs improvement
+vpeRange :: CountVec -> CountVec -> (Ratio Count, Ratio Count)
+''  vs reps =
+   (maximum $ map (uncurry (%) . second (1+)) $ toList l,
+      minimum $ map (uncurry (%)) $ filter ((>0).snd) $ toList l)
+   where l = VG.zip vs reps
+
+--  not useful by itself, see next function
+changes :: CountVec -> CountVec -> [(Ratio Count, (Cand, Cand))]
+''  votes evs = do
+   let ct = VG.length evs
+   fr <- [0 .. ct - 1]
+   let frv = fromIntegral $ votes VG.! fr; fre = evs VG.! fr
+   guard $ fre > 0
+   to <- [0 .. ct - 1]
+   guard $ fr /= to
+   let tov = fromIntegral $ votes VG.! to; toe = evs VG.! to
+   let ratio = fre % (toe + 1)
+   pure ((frv - ratio * tov) / (ratio + 1), (fr, to))
+
+leastChange :: CountVec -> CountVec -> (Ratio Count, (Cand, Cand))
+''  = (minimum .) . changes 
+
+--  Same as above but tests that it is correct.
+leastChange' :: CountVec -> CountVec -> (Ratio Count, (Cand, Cand))
+''  vs evs = run test1 `seq` run test2 `seq` ans where
+   run test = if test then () else error "test failed"
+   staysSame ch f t =
+      apportion etot (vs VG.// [(f, vs VG.! f - ch), (t, vs VG.! t + ch)]) == evs
+   ans@(chg, (fr, to)) = leastChange vs evs
+   chgup = floor $ chg + 1
+   chgdn = ceiling $ chg - 1
+   etot = VG.sum evs
+   test1 = not $ staysSame chgup fr to
+   test2 = and [ staysSame chgdn f t
+               | let rg = [0 .. VG.length vs - 1]
+               , f <- rg, vs VG.! f >= chgdn, t <- rg, f /= t ]
+
+
+--  Parsers.  Arguably could be in a separate module.
+
+splitOn :: Char -> String -> [String]
+''  c s  = let (x, y) = span (/=c) s in x : case y of "" -> []; _:z -> splitOn c z
+
+readVoteLine :: String -> (Label, PECInput)
+''  row =
+      let st : cols = splitOn ',' row
+          h:t = map read' cols
+      in (take 2 st, PECInput h $ VG.fromList t)
+   where read' s = case s of c:_ | isDigit c -> read s; _ -> 0
+
+readVoteSet :: String -> [(Label, PECInput)]
+''  s = map readVoteLine $ filter (\x -> take 1 x /= "#") $ lines s
+
+readVoteFile :: String -> IO [(Label, PECInput)]
+''  fn = readVoteSet <$> readFile fn
+
+addVec :: CountVec -> CountVec -> CountVec
+''  v1 v2 = if VG.length v1 > VG.length v2 then add v1 v2 else add v2 v1 where
+   add w1 w2 = VG.unsafeAccum (+) w1 $ zip [0..] (toList w2)
+
---
+++ apportion.hs
@@ -0,0 +1,57 @@
+--+
+import BasePrelude
+import Voting.PEC
+
+atLarges :: Count
+''  = 2
+
+--  Pick a reasonably round number in the interval.
+roundish :: (Count, Count) -> Count
+''  (lo, hi) | hi < lo = hi -- no integer
+             | = rto mul
+   where
+   rto m = (hi `div` m) * m
+   pow = last $ takeWhile (\p -> rto p >= lo) $ iterate (*10) 1
+   mul | rto (5*pow) >= lo = 5*pow
+       | rto (2*pow) >= lo = 2*pow
+       |                   = pow
+
+formatNum :: Count -> String
+''  ct = reverse $ loop $ reverse $ show ct where
+   loop ds = let (a, b) = splitAt 3 ds
+             in a <> if null b then "" else ',' : loop b
+
+main = do
+	file : _ <- getArgs
+	list <- readVoteFile file
+   tot <- newIORef mempty
+   let chfor w = toEnum $ fromEnum 'A' + w :: Char
+   let printrow = mapM_ (printf " %4d")
+   do
+      printf "   %4c   " 't'
+      let (_, PECInput _ v1) : _ = list
+      forM_ ([0 .. length v1 - 1]) $ printf " %4c" . chfor
+      printf "   %11s   %14c\n" ("v/e" :: String) 'Δ'
+	forM_ list \ (st, inp@(PECInput ev vs)) -> do
+      let apports = apportion (ev - atLarges) vs
+      let evs = addAtLarge atLarges inp apports
+      modifyIORef' tot (addVec evs)
+      printf "%s %4d   " st ev
+      printrow evs
+      let tie = sum evs /= ev
+      let vpe = let (a, b) = vpeRange vs apports; a' = floor a
+                in if tie then a' else roundish (a' + 1, floor b)
+      printf " %13s" $ formatNum vpe
+      if tie then printf " %12c *→*" '1'
+      else do
+         let (chg, (fr, to)) = leastChange vs apports
+         printf " %12s %c→%c" (formatNum $ floor $ 1 + chg) (chfor fr) (chfor to)
+         case
+            let mx = maximum vs
+            in filter (\x -> 2*chg - (mx-x)%1 > 0) $ toList vs of
+               _:_:_ -> putStr " *"
+               _     -> pure ()
+      putStrLn ""
+   tot <- readIORef tot
+	printf "t: %4d   " (sum tot) *> printrow tot *> putStrLn ""
+
